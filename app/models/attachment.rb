1f9c0a6576df7371f0785148f27f665d36414e33
XiangPeng <koalaxp8@gmail.com>
2013-07-19 13:54:06 +0800

    skip image resizing if a file is not an image

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,10 @@
 class Attachment < ActiveRecord::Base
   attr_accessible :attach
   has_attached_file :attach, :styles => {:thumb => "120x120"}
-  validates_attachment_content_type :attach, content_type: ['image/jpeg', 'image/png'], allow_blank: true
+  before_post_process :image?
+  validates_attachment_content_type :attach, content_type: ['image/jpeg', 'image/png'], allow_blank: true, if: :image?
   validates :app_name, inclusion: { in: ApiKey.pluck(:app_name) }
+  def image?
+    !(attach_content_type =~ /^image/).nil?
+  end
 end
